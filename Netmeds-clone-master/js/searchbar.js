13333780db50a019f1152bb77aef5e0db236991f
sandeepnilaji <snilaji@gmail.com>
2021-08-29 16:46:51 +0530

    add cart btn added

--- Netmeds-clone-master/js/searchbar.js
+++ Netmeds-clone-master/js/searchbar.js
@@ -34,13 +34,56 @@ function appendData(meds) {
     let p = document.createElement("p");
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
+    let bo = document.createElement("button");
+    bo.innerText = "Add tO cart";
     p.innerText = MedicineName;
     p1.innerText = TypeofSell;
-    p2.innerText = "RS " + MRP;
+    p2.innerText = MRP;
     img.src = img_m;
     img_div.append(img);
-    cont_div.append(p, p1, p2);
+    bo.className = "addBT";
+    bo.addEventListener("click", function () {
+      addToCart({ name: MedicineName, d_price: MRP, image: img_m });
+    });
+
+    cont_div.append(p, p1, p2, bo);
     mini.append(img_div, cont_div);
     main.append(mini);
   });
+}
+function addToCart(obj) {
+  let arr;
+
+  arr = localStorage.getItem("visited");
+
+  if (arr == null) {
+    arr = [];
+
+    arr.push(obj);
+
+    alert("Item added to cart successfully");
+  } else {
+    arr = JSON.parse(localStorage.getItem("visited"));
+
+    var flag = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].name == obj.name) {
+        flag = 1;
+
+        break;
+      }
+    }
+
+    if (flag == 0) {
+      arr.push(obj);
+
+      alert("Item added to cart successfully");
+    }
+    if (flag == 1) {
+      alert("Item already present in cart");
+    }
+  }
+
+  localStorage.setItem("visited", JSON.stringify(arr));
 }
